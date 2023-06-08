ed3fbf135f78c4d624efc4ca0c5a112dd2dcf7c1
FzeroH <vampir666x@gmail.com>
2023-06-08 07:14:13 +0300

    Добавил в ID пользователя и кафедры

--- src/types/publication.ts
+++ src/types/publication.ts
@@ -74,6 +74,8 @@ export type PublicationFullInfo = {
 export type CreateOrderRequest = {
   pub_type_id: number
   publication_id: number
+  cafedra_id: number
+  users_id: number
   students_discipline_ids: number[]
   request_count: number
 }
